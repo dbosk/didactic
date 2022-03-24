8e9468ef5888aa584264a2eb224faf16136560a4
Daniel Bosk <dbosk@kth.se>
2022-03-24 12:52:08 +0100

    Adapts make files to didactic name change too

---
+++ didactic.mk
@@ -0,0 +1,20 @@
+didactic.sty: ${INCLUDE_DIDACTIC}/didactic.sty
+	${LN} $^ $@
+
+
+.PHONY: ${INCLUDE_DIDACTIC}/didactic.sty
+
+${INCLUDE_DIDACTIC}/didactic.sty:
+	${MAKE} -C $(dir $@) $(notdir $@)
+
+
+.PHONY: clean clean-didactic
+clean: clean-didactic
+clean-didactic:
+	${RM} didactic.sty
+
+.PHONY: distclean distclean-didactic
+distclean: distclean-didactic
+distclean-didactic:
+	${RM} didactic.sty
+	${MAKE} -C ${INCLUDE_DIDACTIC} clean distclean
