e93aef70e5e97f96c5ad5ffcf137a66e033bc17d
Daniel Bosk <dbosk@kth.se>
2024-03-17 14:51:48 +0100

    Improves code, adds translations

--- Makefile
+++ Makefile
@@ -2,15 +2,18 @@ LATEX?=latex
 LATEXFLAGS=-shell-escape
 
 .PHONY: all
-all: didactic.sty didactic.pdf
+all: didactic.sty didactic.pdf test.pdf
 
-didactic.pdf: didactic.dtx
+didactic.pdf: didactic.dtx didactic.sty
 didactic.sty: didactic.ins
+
+test.pdf: test.tex didactic.sty
 
 .PHONY: clean
 clean:
 	${RM} didactic.sty didactic.pdf
 	${RM} didactic.log
+	${RM} test.pdf
 
 
 INCLUDE_MAKEFILES=makefiles
--- didactic.mk
+++ didactic.mk
@@ -1,6 +1,12 @@
 didactic.sty: ${INCLUDE_DIDACTIC}/didactic.sty
 	${LN} $^ $@
 
+didactic.sty: didactic-Swedish.dict didactic-English.dict
+
+didactic-Swedish.dict: ${INCLUDE_DIDACTIC}/didactic-Swedish.dict
+	${LN} $^ $@
+didactic-English.dict: ${INCLUDE_DIDACTIC}/didactic-English.dict
+	${LN} $^ $@
 
 .PHONY: ${INCLUDE_DIDACTIC}/didactic.sty
 
@@ -12,9 +18,9 @@ ${INCLUDE_DIDACTIC}/didactic.sty:
 clean: clean-didactic
 clean-didactic:
 	${RM} didactic.sty
+	${RM} didactic-Swedish.dict didactic-English.dict
 
 .PHONY: distclean distclean-didactic
 distclean: distclean-didactic
 distclean-didactic:
-	${RM} didactic.sty
 	${MAKE} -C ${INCLUDE_DIDACTIC} clean distclean
