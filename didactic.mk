e3b06f12a28ecca22905dd0b7c3a387611321d58
Daniel Bosk <dbosk@kth.se>
2024-03-20 09:17:51 +0100

    Makes translations work better
    - Changes \if's to be positive
    
    The options were negated, which easily leads to errors (which it did).
    Even if the option is `nobeamer` the code should be
    `\if@didactic@beamer` instead of `\if@didactic@nobeamer`.
    
    - Changes to use `translations` instead of `translator`
    
    This way we can use the normal `babel` way and not have to provide the
    languages as options to the class.
    
    - Adds use of `\crefname` and `\Crefname` to fix bug
    
    Without the use of `\crefname` and `\Crefname` the translations don't
    work for `refname` and `Refname` in `thmtools`.

--- didactic.mk
+++ didactic.mk
@@ -1,16 +1,7 @@
 didactic.sty: ${INCLUDE_DIDACTIC}/didactic.sty
-	${LN} $^ $@
+	${LN} $< $@
 
-didactic.sty: didactic-Swedish.dict didactic-English.dict
-
-didactic-Swedish.dict: ${INCLUDE_DIDACTIC}/didactic-Swedish.dict
-	${LN} $^ $@
-didactic-English.dict: ${INCLUDE_DIDACTIC}/didactic-English.dict
-	${LN} $^ $@
-
-.PHONY: ${INCLUDE_DIDACTIC}/didactic.sty
-
-${INCLUDE_DIDACTIC}/didactic.sty:
+${INCLUDE_DIDACTIC}/didactic.sty::
 	${MAKE} -C $(dir $@) $(notdir $@)
 
 
@@ -18,7 +9,6 @@ ${INCLUDE_DIDACTIC}/didactic.sty:
 clean: clean-didactic
 clean-didactic:
 	${RM} didactic.sty
-	${RM} didactic-Swedish.dict didactic-English.dict
 
 .PHONY: distclean distclean-didactic
 distclean: distclean-didactic
