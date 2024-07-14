0f2010ab3d8bafaa0390757d7006779e20bd2c44
Daniel Bosk <dbosk@kth.se>
2024-07-14 22:44:25 +0200

    Makes Python part a separate Python package

--- didactic.mk
+++ didactic.mk
@@ -1,14 +1,16 @@
-didactic.sty: ${INCLUDE_DIDACTIC}/didactic.sty
+DIDACTIC_STY=/tex/didactic.sty
+
+didactic.sty: ${INCLUDE_DIDACTIC}/${DIDACTIC_STY}:
 	${LN} $< $@
 
-${INCLUDE_DIDACTIC}/didactic.sty::
+${INCLUDE_DIDACTIC}/${DIDACTIC_STY}:
 	${MAKE} -C $(dir $@) $(notdir $@)
 
 
 .PHONY: clean clean-didactic
 clean: clean-didactic
 clean-didactic:
-	${RM} didactic.sty
+	${MAKE} -C ${INCLUDE_DIDACTIC} clean
 
 .PHONY: distclean distclean-didactic
 distclean: distclean-didactic
